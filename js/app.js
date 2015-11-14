9fed6e522f7957723c7fde9b17b92809dbe5af22
Ankit Famyial <informankit@ymail.com>
2015-11-15 01:59:51 +0530

    Setup foundation for application

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation({
+  orbit: {
+    timer_speed: 5000,
+     pause_on_hover: false,
+     animation_speed: 2000,
+     variable_height: false,
+     stack_on_small: false
+  }
+});
