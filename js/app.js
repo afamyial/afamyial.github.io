a5317129c2ef1deaa468d05723ec5ed38dd53218
Ankit Famyial <informankit@ymail.com>
2015-11-15 19:12:14 +0530

    Add Nav bar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation({
-  orbit: {
-    timer_speed: 5000,
-     pause_on_hover: false,
-     animation_speed: 2000,
-     variable_height: false,
-     stack_on_small: false
-  }
-});
+$(document).foundation();
